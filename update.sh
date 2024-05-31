279091402e34207ec8a6764efa62422d905d9a14
UmmIt <git@ummit.dev>
2024-05-31 20:31:58 +0800

    feat: Building Hugo site

--- update.sh
+++ update.sh
@@ -6,7 +6,15 @@ GRAY='\033[1;30m'
 DARKRED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# Function for Hugo building
+build_hugo() {
+    echo -e "${GREEN}[+] Building Hugo site...${NC}"
+    hugo --minify
+    echo -e "${GREEN}[+] Hugo build complete.${NC}"
+}
+
 valid_choice=false
+do_hugo=false
 
 while [ "$valid_choice" == false ]; do
     # Check if --noconfirm is passed as a command-line argument
@@ -14,7 +22,7 @@ while [ "$valid_choice" == false ]; do
         choice="y"
     else
         # Prompt user for confirmation
-        read -p "Do you want to update submodules? [Y/n]: " choice
+        read -p "Do you want to update Hugo site and submodules? [Y/n]: " choice
         choice=${choice:-y} # Default to 'yes' if user just presses Enter
     fi
 
@@ -23,11 +31,12 @@ while [ "$valid_choice" == false ]; do
             # Update submodules recursively
             echo -e "${GREEN}[+] Updating submodules...${NC}"
             git submodule update --init --recursive --remote
-            echo -e "${GREEN}[+] Updated complete.${NC}"
+            echo -e "${GREEN}[+] Updated submodules complete.${NC}"
             valid_choice=true
+            do_hugo=true
             ;;
         n|N ) 
-            echo -e "${GRAY}[~] No submodule update performed.${NC}"
+            echo -e "${GRAY}[~] No update performed.${NC}"
             valid_choice=true
             ;;
         * ) 
@@ -35,3 +44,8 @@ while [ "$valid_choice" == false ]; do
             ;;
     esac
 done
+
+# Build Hugo site after updating submodules if --hugo option is provided
+if [ "$do_hugo" = true ]; then
+    build_hugo
+fi
